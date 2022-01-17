c4c8fb1d2846a957a9ea89bd6e03b79651fca997
Gabriela Papp <gpgabika@gmail.com>
2022-01-17 21:38:55 +0000

    api change

--- src/Main.js
+++ src/Main.js
@@ -1,43 +1,55 @@
 import React from 'react'
-import { useGlobalContext } from './context';
+import { useGlobalContext } from './context'
 import Restaurant from './Restaurant'
 import Map from './Map'
 import RecommendedVenues from './RecommendedVenues'
-import { ImShare } from "react-icons/im";
-import { AiFillContacts } from "react-icons/ai";
-import { IoMdHeartEmpty } from "react-icons/io";
+import { ImShare } from 'react-icons/im'
+import { AiFillContacts } from 'react-icons/ai'
+import { IoMdHeartEmpty } from 'react-icons/io'
 
+function Main() {
+  const { location, venues } = useGlobalContext()
 
-function Main() { 
-    const { location, venues} = useGlobalContext();
-  
-    return (
-        <div>
-            <Map
-                center={location}
-                markers={venues}
-            />
-            <div className='info-suggest'>
-                <div className="empty-section"></div>
-                <div className="info">
-                    <div className='info-section'>
-                        <i><ImShare /></i><p><span>Share. </span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, eius.</p>
-                    </div>
-                    <div className='info-section'>
-                        <i><IoMdHeartEmpty /></i><p><span>Can't decide? </span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, corrupti.</p>
-                    </div>
-                    <div className='info-section'>
-                        <i><AiFillContacts /></i><p><span>Save your favorite places. </span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, tempore.</p>
-                    </div>
-                </div>
-                <div className="suggest">
-
-                </div>
-            </div>
-            <Restaurant venues={venues}/>
-            <RecommendedVenues/>
+  return (
+    <div>
+      <Map center={location} markers={venues} />
+      <div className="info-suggest">
+        <div className="empty-section"></div>
+        <div className="info">
+          <div className="info-section">
+            <i>
+              <ImShare />
+            </i>
+            <p>
+              <span>Share. </span>Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Praesentium, eius.
+            </p>
+          </div>
+          <div className="info-section">
+            <i>
+              <IoMdHeartEmpty />
+            </i>
+            <p>
+              <span>Can't decide? </span>Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Asperiores, corrupti.
+            </p>
+          </div>
+          <div className="info-section">
+            <i>
+              <AiFillContacts />
+            </i>
+            <p>
+              <span>Save your favorite places. </span>Lorem ipsum dolor sit amet
+              consectetur adipisicing elit. Quod, tempore.
+            </p>
+          </div>
+        </div>
+        <div className="suggest"></div>
+      </div>
+      <Restaurant venues={venues} />
+      <RecommendedVenues />
     </div>
-    )
+  )
 }
 
 export default Main
--- src/context.js
+++ src/context.js
@@ -1,89 +1,115 @@
-import React, { useState, useContext,useEffect } from 'react'
-import axios from 'axios';
-const AppContext = React.createContext();
+import React, { useState, useContext, useEffect } from 'react'
+import axios from 'axios'
+const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
-    const [venues, setVenues] = useState([])
-    const [input, setInput] = useState('')
-    const [location, setLocation] = useState({ lat: null, lng: null })
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const [isCatModalOpen, setIsCatModalOpen] = useState(false);
-    const [recommendedVenues, setRecommendedVenues] = useState([])
-    const [recommendedVenueId, setRecommendedVenueId] = useState([])
-    const [getByCategory,setGetByCategory]=useState([])
+  const [venues, setVenues] = useState([])
+  const [input, setInput] = useState('')
+  const [location, setLocation] = useState({ lat: null, lng: null })
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [isCatModalOpen, setIsCatModalOpen] = useState(false)
+  const [recommendedVenues, setRecommendedVenues] = useState([])
+  const [recommendedVenueId, setRecommendedVenueId] = useState([])
+  const [getByCategory, setGetByCategory] = useState([])
 
-    useEffect(() => {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(function (position) {
-                setLocation({ lat: position.coords.latitude, lng: position.coords.longitude })
-            });
-        }
-    }, [])
-
-    useEffect(() => {
-        axios.get(`https://api.foursquare.com/v2/venues/explore?client_id=LIEUSSAMWMOVTOT2NMXPTPQ1VJ5UIESU3EJKN53JV4QIKMZL&client_secret=FURPJP2L1EQJHTT3Y05RAEJOOLBPXTMALC1OJ3NQ2LAHYT5G&ll=${location.lat},${location.lng}&query=food&v=20189988&limit=9`)
-            .then(res => {
-                const recomVenues = res.data.response.groups;
-
-                setRecommendedVenues(recomVenues)
-
-            })
-            .catch(err => {
-                console.log(err)
-            })
-    }, [location.lat, location.lng])
-
-    const getVenueId =(id)=>{
-        setRecommendedVenueId(id)
-        alert(recommendedVenueId)
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        setLocation({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        })
+      })
     }
+  }, [])
 
-    const onTextSubmit = (e) => {
-       e.preventDefault()
-        axios.get(`https://api.foursquare.com/v2/venues/search?client_id=LIEUSSAMWMOVTOT2NMXPTPQ1VJ5UIESU3EJKN53JV4QIKMZL&client_secret=FURPJP2L1EQJHTT3Y05RAEJOOLBPXTMALC1OJ3NQ2LAHYT5G&ll=${location.lat},${location.lng}&query=${input}&v=20189988&limit=9`)
-            .then(res => {
-                const venues = res.data.response.venues;
-                setVenues(venues)
-                setInput('')
-            })
-            .catch(err => {
-                console.log(err)
-            })
-        
+  const URL =
+    'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary'
+  var options = {
+    params: {
+      bl_latitude: '11.847676',
+      tr_latitude: '12.838442',
+      bl_longitude: '109.095887',
+      tr_longitude: '109.149359',
+    },
+    headers: {
+      'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
+      'x-rapidapi-key': 'b9a7e73397msh978d4b617a4ff92p13c79ejsn03bd956de277',
+    },
+  }
+  const getPlacesData = async () => {
+    try {
+      const {
+        data: { data },
+      } = await axios.get(URL, options)
+      return data
+    } catch (error) {
+      console.log(error)
     }
+  }
 
-    const openModal = () => {
-        setIsModalOpen(true);
-    }
-    const closeModal = () => {
-        setIsModalOpen(false);
-    }
-    const openCatModal = () => {
-        setIsCatModalOpen(true);
-    }
-    const closeCatModal = () => {
-        setIsCatModalOpen(false);
-    }
-    const handleCategory =(id)=>{
-        axios.get(`https://api.foursquare.com/v2/venues/similar?${id}&client_id=LIEUSSAMWMOVTOT2NMXPTPQ1VJ5UIESU3EJKN53JV4QIKMZL&client_secret=FURPJP2L1EQJHTT3Y05RAEJOOLBPXTMALC1OJ3NQ2LAHYT5G`)
-            .then(res => {
-                const byCategory = res.data.response;
+  useEffect(() => {
+    getPlacesData()
+  }, [])
 
-                setGetByCategory(byCategory)
-                console.log(getByCategory)
-            })
-            .catch(err => {
-                console.log(err)
-            })
-    }
+  const getVenueId = (id) => {
+    setRecommendedVenueId(id)
+    alert(recommendedVenueId)
+  }
 
-    return <AppContext.Provider value={{
+  const onTextSubmit = (e) => {
+    e.preventDefault()
+    axios
+      .get(
+        `https://api.foursquare.com/v2/venues/search?client_id=LIEUSSAMWMOVTOT2NMXPTPQ1VJ5UIESU3EJKN53JV4QIKMZL&client_secret=FURPJP2L1EQJHTT3Y05RAEJOOLBPXTMALC1OJ3NQ2LAHYT5G&ll=${location.lat},${location.lng}&query=${input}&v=20189988&limit=9`,
+      )
+      .then((res) => {
+        const venues = res.data.response.venues
+        setVenues(venues)
+        setInput('')
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+  const openModal = () => {
+    setIsModalOpen(true)
+  }
+  const closeModal = () => {
+    setIsModalOpen(false)
+  }
+  const openCatModal = () => {
+    setIsCatModalOpen(true)
+  }
+  const closeCatModal = () => {
+    setIsCatModalOpen(false)
+  }
+  const handleCategory = (id) => {
+    axios
+      .get(
+        `https://api.foursquare.com/v2/venues/similar?${id}&client_id=LIEUSSAMWMOVTOT2NMXPTPQ1VJ5UIESU3EJKN53JV4QIKMZL&client_secret=FURPJP2L1EQJHTT3Y05RAEJOOLBPXTMALC1OJ3NQ2LAHYT5G`,
+      )
+      .then((res) => {
+        const byCategory = res.data.response
+
+        setGetByCategory(byCategory)
+        console.log(getByCategory)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+  return (
+    <AppContext.Provider
+      value={{
         venues,
         setVenues,
-        location, 
+        location,
         onTextSubmit,
         input,
-         setInput,
+        setInput,
         openModal,
         closeModal,
         openCatModal,
@@ -92,16 +118,16 @@ const AppProvider = ({ children }) => {
         isModalOpen,
         recommendedVenues,
         getVenueId,
-        handleCategory
-    }}>
-        {children}
+        handleCategory,
+      }}
+    >
+      {children}
     </AppContext.Provider>
-
+  )
 }
 
 export const useGlobalContext = () => {
-    return useContext(AppContext);
+  return useContext(AppContext)
 }
 
-export { AppContext, AppProvider };
-
+export { AppContext, AppProvider }
