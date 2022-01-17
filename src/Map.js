3ce21e40bf1c9f7f41cfc0eff30cecd3d4b2f62a
Gabriela Papp <gpgabika@gmail.com>
2022-01-17 20:53:46 +0000

    maps component changed

--- src/Map.js
+++ src/Map.js
@@ -1,42 +1,45 @@
-import { GoogleMap, LoadScript  } from '@react-google-maps/api';
-import { Marker } from '@react-google-maps/api';
-import { useGlobalContext } from './context';
+import GoogleMapReact from 'google-map-react'
+import { Marker } from '@react-google-maps/api'
+import { useGlobalContext } from './context'
 
 const containerStyle = {
-    width: '100%',
-    height: '400px'
-};
-function Map({center}) {
-    const { venues, recommendedVenueId } = useGlobalContext();
+  width: '100%',
+  height: '400px',
+}
+function Map({ center }) {
+  const { venues, recommendedVenueId } = useGlobalContext()
+  const coords = { lat: 0, lng: 0 }
+  const { REACT_APP_GOOGLE_MAPS_API } = process.env
 
-    const mapMarkers = venues.map((venue, i) => {
-        const marker = {
-            position: {
-                lat: venue.location.lat,
-                lng: venue.location.lng
-            },
-            animation:{
-                animation:true
-            }
-        }
-            return <Marker key={venue.id} {...marker} onClick={()=>alert(venue.id)}/>
-      
-    })
-   
-    return (
-        <LoadScript
-            googleMapsApiKey=""
-        >
-            <GoogleMap
-                mapContainerStyle={containerStyle}
-                center={center}
-                zoom={11}
-            >
-                <div>{mapMarkers}</div>
-            </GoogleMap>
-        </LoadScript>
+  const mapMarkers = venues.map((venue, i) => {
+    const marker = {
+      position: {
+        lat: venue.location.lat,
+        lng: venue.location.lng,
+      },
+      animation: {
+        animation: true,
+      },
+    }
+    return <Marker key={venue.id} {...marker} />
+  })
 
-    )
+  return (
+    <div style={containerStyle}>
+      <GoogleMapReact
+        mapContainerStyle={containerStyle}
+        bootstrapURLKeys={{ key: REACT_APP_GOOGLE_MAPS_API }}
+        defaultCenter={coords}
+        center={center}
+        defaultZoom={11}
+        options={''}
+        onChange={''}
+        onChildClick={''}
+      >
+        <div>{mapMarkers}</div>
+      </GoogleMapReact>
+    </div>
+  )
 }
 
 export default Map
